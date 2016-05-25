d1d98a340bc04573eeb619204b6e9f2b31780932
shenzhaoliang <409178030@qq.com>
2016-05-25 15:52:59 +0800

    2016年05月25日15:52:43 2 StructMutable

---
+++ Swift2Guide/Swift100Tips/Swift100Tips/StructMutableController.swift
@@ -0,0 +1,25 @@
+//
+//  StructMutableController.swift
+//  Swift100Tips
+//
+//  Created by rkxt_ios on 16/5/25.
+//  Copyright © 2016年 ST. All rights reserved.
+//
+
+import UIKit
+
+class StructMutableController: UIViewController {
+
+
+}
+
+struct User {
+    var age: Int
+    var name: String
+    var height: Int
+    
+    mutating func gainWeight(newWeight: Int) {
+        height += newWeight
+    }
+}
+
